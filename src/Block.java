4820d68aa6170c48ed2132bce8ac177ace7c4f7d
samzechman <zechsp19@wfu.edu>
2020-12-01 16:20:06 -0500

    more progress

--- src/Block.java
+++ src/Block.java
@@ -1,9 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Block {
@@ -51,13 +52,13 @@ public class Block {
     //calculate block hash
     public String calculateBlockHash() {
         String dataToHash = prevHash
-                + timestamp //ask about in class
+                + timestamp
                 + Integer.toString(nonce)
                 + data.toString();
         MessageDigest digest = null;
         byte[] bytes = null;
         try {
-            digest = MessageDigest.getInstance("SHA-256"); //ask about in class
+            digest = MessageDigest.getInstance("SHA-256");
             bytes = digest.digest(dataToHash.getBytes("UTF-8"));
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
             System.out.println("The encoding is not supported");
@@ -71,37 +72,122 @@ public class Block {
 
     //method to mine the blocks
     public String mineBlock(int prefix){
+        //Define the prefix you desire to find
         String prefixString = new String(new char[prefix]).replace('\0', '0');
-        if (TreatySC(t)){
-
+        //If the transaction meets the stakeholders agreement in TreatySC, mine the block;
+        //otherwise abort the transaction and display a proper message.
+        if (!TreatySC(data)){
+            System.out.println("This transaction does not meet the stakeholders' agreement -- block not valid.");
         }
         else {
+            //Check whether you've found the solution
+            //If not, increment the nonce and calculate the hash in a loop
             while (!currHash.substring(0, prefix).equals(prefixString)) {
                 nonce++;
                 currHash = calculateBlockHash();
             }
-            return currHash;
         }
+        return currHash;
     }
 
     //implement the agreement between the stakeholders
     public boolean TreatySC (Transaction t){
-        return true;
+        if (retrieveProvenance(data.getArtefact().getId(), 2001).size() >= 2 && data.getBuyer().getBalance() >= data.getPrice()){
+            //10% of sale given to the auction house
+            data.getAuctionHouse().setBalance(data.getAuctionHouse().getBalance() + data.getPrice() * 0.1);
+            //20% of sale to country of origin
+            /////ASK ABOUT IN CLASS
+            //70% of sale given to the seller
+            data.getSeller().setBalance(data.getSeller().getBalance() + data.getPrice() * 0.7);
+            return true;
+        }
+        return false;
     }
 
     //retrieve all transactions in the block chain for the artefact identified by its id
-    public void retrieveProvenance (String id){
+    public ArrayList<Transaction> retrieveProvenance (String id){
+        FileInputStream transactionsFile = null;
+        //ensure the file actually exists
+        try {
+            transactionsFile = new FileInputStream("src/transactionsFile");
+        }
+        //if file does not exist, end program
+        catch (FileNotFoundException e) {
+            System.out.println("Could not find and open file - exiting code; please enter the correct file name");
+            System.exit(1);
+        }
+        //if file does exist, continue program using file
+        Scanner fileReader = new Scanner(transactionsFile);
 
+        //create array for the transactions
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        //parse the text file of transactions
+        while(fileReader.hasNextLine()){
+            //if the id provided matches the artefact's id, add to the array
+            if (data.getArtefact().getId() == id){
+                transactions.add(data);
+            }
+        }
+        return transactions;
     }
 
     //retrieve all transactions in the block chain for the artefact identified by id after the date and time given through timestamp
-    public void retrieveProvenance (String id, long timestamp){
+    public ArrayList<Transaction> retrieveProvenance (String id, int timestamp){
+        FileInputStream transactionsFile = null;
+        //ensure the file actually exists
+        try {
+            transactionsFile = new FileInputStream("src/transactionsFile");
+        }
+        //if file does not exist, end program
+        catch (FileNotFoundException e) {
+            System.out.println("Could not find and open file - exiting code; please enter the correct file name");
+            System.exit(1);
+        }
+        //if file does exist, continue program using file
+        Scanner fileReader = new Scanner(transactionsFile);
 
+        //create array for the transactions
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        //parse the text file of transactions
+        while(fileReader.hasNextLine()){
+            //if the id provided matches the artefact's id, add to the array
+            if (data.getArtefact().getId() == id && data.getTimestamp().getYear() > timestamp){
+                transactions.add(data);
+            }
+        }
+        return transactions;
     }
 
     //can be used by any node in the network to validate that a block chain is valid
     public boolean verify_Blockchain (ArrayList<Block> BC){
 
+        Block currentBlock;
+        Block previousBlock;
+        //iterating through all the blocks
+        for (int i = 1; i < BC.size(); i++) {
+            //storing the current block and the previous block
+            currentBlock = BC.get(i);
+            previousBlock = BC.get(i - 1);
+
+            //checking if the current hash is equal to the calculated hash or not
+            if (!currentBlock.currHash.equals(currentBlock.calculateBlockHash())) {
+                //hashes not equal
+                return false;
+            }
+            //checking if the previous hash is equal to the calculated previous hash or not
+            if (!previousBlock.currHash.equals(currentBlock.prevHash)) {
+                //prev hashes not equal
+                return false;
+            }
+            //checking if the current block has been mined
+            if ( currentBlock.getCurrHash().substring(0, prefix).equals(prefixString) ) { ////ASK IN CLASS
+                //block has not been mined
+                return false;
+            }
+        }
+        // If all the hashes are equal to the calculated hashes, then blockchain is valid
         return true;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main (String[] args){
@@ -8,29 +12,150 @@ public class Main {
         String prefixString = new String(new char[prefix]).replace('\0', '0');
         Scanner scnr = new Scanner(System.in);
 
+        //data1 - data3 created
+        Transaction data1 = new Transaction();
+        Transaction data2 = new Transaction();
+        Transaction data3 = new Transaction();
 
-        //data1-data3 should be filled by the user
-        System.out.println("Enter artefact: ");
+        FileInputStream stakeholdersFile = null;
+        //ensure the file actually exists
+        try {
+            stakeholdersFile = new FileInputStream("src/stakeholdersFile");
+        }
+        //if file does not exist, end program
+        catch (FileNotFoundException e) {
+            System.out.println("Could not find and open file - exiting code; please enter the correct file name");
+            System.exit(1);
+        }
+        //if file does exist, continue program using file
 
+        Scanner fileReader = new Scanner(stakeholdersFile);
+
+        //read in the first line of variables, pass over to get to the actual data
+        String header = fileReader.nextLine();
+
+        //read in stakeholder data
+        while (fileReader.hasNextLine()) {
+            //creates the Stakeholder objects
+            Stakeholder s = new Stakeholder();
+
+            //read in next line
+            String text = fileReader.nextLine();
+
+            //parse by ","
+            String[] splits = text.split(",");
+
+            //set private variables
+            s.setId(splits[0]);
+            s.setName(splits[1]);
+            s.setAddress(splits[2]);
+            s.setBalance(Double.parseDouble(splits[3]));
+
+            //fill in data1 - data3
+            if (data1.getBuyer() == null){
+                data1.setBuyer(s);
+            }
+            else if (data1.getSeller() == null){
+                data1.setSeller(s);
+            }
+            else if (data1.getAuctionHouse() == null){
+                data1.setAuctionHouse(s);
+            }
+            else if (data2.getBuyer() == null){
+                data2.setBuyer(s);
+            }
+            else if (data2.getSeller() == null){
+                data2.setSeller(s);
+            }
+            else if (data2.getAuctionHouse() == null){
+                data2.setAuctionHouse(s);
+            }
+            else if (data3.getBuyer() == null){
+                data3.setBuyer(s);
+            }
+            else if (data3.getSeller() == null){
+                data3.setSeller(s);
+            }
+            else if (data3.getAuctionHouse() == null){
+                data3.setAuctionHouse(s);
+                break;
+            }
+        }
+        fileReader.close();
+
+        FileInputStream artefactsFile = null;
+        //ensure the file actually exists
+        try {
+            artefactsFile = new FileInputStream("src/artefactsFile");
+        }
+        //if file does not exist, end program
+        catch (FileNotFoundException e) {
+            System.out.println("Could not find and open file - exiting code; please enter the correct file name");
+            System.exit(1);
+        }
+        //if file does exist, continue program using file
+
+        fileReader = new Scanner(artefactsFile);
+
+        //read in the first line of variables, pass over to get to the actual data
+        header = fileReader.nextLine();
+
+        //read in artefact data
+        while (fileReader.hasNextLine()) {
+            //creates the Artefact objects
+            Artefact a = new Artefact();
+
+            //read in next line
+            String text = fileReader.nextLine();
+
+            //parse by ","
+            String[] splits = text.split(",");
+
+            //set private variables
+            a.setId(splits[0]);
+            a.setName(splits[1]);
+            a.setCountry(splits[2]);
+
+            //fill in data1 - data3
+            if (data1.getArtefact() == null){
+                a.setOwner(data1.getSeller());
+                data1.setArtefact(a);
+            }
+            else if (data2.getArtefact() == null){
+                a.setOwner(data2.getSeller());
+                data2.setArtefact(a);
+            }
+            else if (data3.getArtefact() == null){
+                a.setOwner(data3.getSeller());
+                data3.setArtefact(a);
+                break;
+            }
+        }
+        fileReader.close();
+
+        //data1-data3 filled from text files
 
 /*
+        //adding first block
         Block genesisBlock = new Block(data1, blockchain.get(blockchain.size() - 1).getCurrHash(), new Date().getTime());
-        newBlock.mineBlock(prefix);
-        if (genesisBlock.getCurrHash().substring(0, prefix).equals(prefixString) && verify_Blockchain(ArrayList<Block> BC))
-            blockchain.add(newBlock);
+        genesisBlock.mineBlock(prefix);
+        if (genesisBlock.getCurrHash().substring(0, prefix).equals(prefixString) && genesisBlock.verify_Blockchain(blockchain))
+            blockchain.add(genesisBlock);
         else
             System.out.println("Malicious block, not added to the chain");
 
-        Block secondBlock = new Block(data2, blockchain.get(blockchain.size() - 1).getHash(), new Date().getTime());
-        newBlock.mineBlock(prefix);
-        if (secondBlock.getCurrHash().substring(0, prefix).equals(prefixString) && verify_Blockchain(ArrayList<Block> BC))
-            blockchain.add(newBlock);
+        //adding second block
+        Block secondBlock = new Block(data2, blockchain.get(blockchain.size() - 1).getCurrHash(), Date().getTime());
+        secondBlock.mineBlock(prefix);
+        if (secondBlock.getCurrHash().substring(0, prefix).equals(prefixString) && secondBlock.verify_Blockchain(blockchain))
+            blockchain.add(secondBlock);
         else
             System.out.println("Malicious block, not added to the chain");
 
+        //adding third block
         Block newBlock = new Block(data3, blockchain.get(blockchain.size() - 1).getCurrHash(), new Date().getTime());
         newBlock.mineBlock(prefix);
-        if (newBlock.getCurrHash().substring(0, prefix).equals(prefixString) && verify_Blockchain(ArrayList<Block> BC))
+        if (newBlock.getCurrHash().substring(0, prefix).equals(prefixString) && newBlock.verify_Blockchain(blockchain))
             blockchain.add(newBlock);
         else
             System.out.println("Malicious block, not added to the chain");
